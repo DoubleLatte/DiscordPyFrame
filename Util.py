8b169eb3f3de6b985c6590a81a470b8360e643b5
DoubleLatte <85533915+DoubleLatte@users.noreply.github.com>
2025-02-08 14:46:24 +0900

    update

---
+++ Util.py
@@ -0,0 +1,8 @@
+'''user_id = str (interaction.user.id)
+        user_name = interaction.user.name
+        user_nick = interaction.user.nick 
+        user_role = str(interaction.user.roles)\
+''' 
+def ID(ineraction):
+    user_id = str (interaction.user.id)
+    return user_id
